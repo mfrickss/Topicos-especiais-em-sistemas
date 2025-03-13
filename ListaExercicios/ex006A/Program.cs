f893bfb0bca5d92ffeb15338b8bdbb7d7ea4fd20
RICARDO AUGUSTO DE CAMARGO <ricardo.camargo03@cs.up.edu.br>
2025-03-13 11:25:20 -0300

    Add projetos

---
+++ ListaExercicios/ex006A/Program.cs
@@ -0,0 +1,55 @@
+﻿//Desenvolver um algoritmo para receber 1000 valores automaticamente dentro de um vetor e ordenar do menor para o maior.
+
+// 1. Desenvolver o algoritmo de ordenação (Bubble Sort);
+// 2. Utilizar uma função em C# para ordenação;
+
+//PASSOS PARA RESOLVER O EXERCÍCIO
+//1 - Criar um vetor de 1000 posições
+//2 - Preencher o vetor com valores aleatórios
+//3 - Imprimir o vetor na tela
+//4 - Ordenar o vetor com o Bubble Sort
+
+//Criar um vetor de valores inteiros
+int[] vetor = new int[1000];
+
+//Gerar um valor randômico
+Random r = new Random();
+
+//Preencher o vetor com valores aleatórios
+for (int i = 0; i < vetor.Length; i++)
+{
+    vetor[i] = r.Next(1000);
+}
+
+//Imprimir o vetor com valores aletatórios
+for (int i = 0; i < vetor.Length; i++)
+{
+    Console.Write(vetor[i] + " ");
+}
+
+//Ordenar o vetor com o Bubble Sort
+bool troca = false;
+do
+{
+    troca = false;
+    for (int i = 0; i < vetor.Length - 1; i++)
+    {
+
+        //Trocar valores
+        if (vetor[i] > vetor[i + 1])
+        {
+            int aux = vetor[i];
+            vetor[i] = vetor[i + 1];
+            vetor[i + 1] = aux;
+            troca = true;
+        }
+    }
+} while (troca == true);
+
+
+Console.WriteLine("\n");
+//Imprimir o vetor com valores ordenados
+for (int i = 0; i < vetor.Length; i++)
+{
+    Console.Write(vetor[i] + " ");
+}
---
+++ ListaExercicios/ex006B/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Criando o vetor com 10 elementos aleatórios
+        Random rand = new Random();
+        int[] numeros = new int[10];
+        
+        // Preenchendo o vetor com números aleatórios entre 1 e 100
+        for (int i = 0; i < numeros.Length; i++)
+        {
+            numeros[i] = rand.Next(1, 101); // números entre 1 e 100
+        }
+        
+        // Exibindo os números antes de ordenar
+        Console.WriteLine("Vetor antes de ordenar:");
+        foreach (int numero in numeros)
+        {
+            Console.Write(numero + " ");
+        }
+        
+        Console.WriteLine(); // Linha em branco
+        
+        // Ordenando o vetor
+        Array.Sort(numeros);
+        
+        // Exibindo os números depois de ordenar
+        Console.WriteLine("Vetor depois de ordenar:");
+        foreach (int numero in numeros)
+        {
+            Console.Write(numero + " ");
+        }
+    }
+}
+
